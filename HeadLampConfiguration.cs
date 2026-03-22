0f531560fe28fb42c9a79ff3be7bf6c939bdf166
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-22 20:46:31 +0300

    Create HeadLampConfiguration.cs

---
+++ HeadLampConfiguration.cs
@@ -0,0 +1,35 @@
+using Rocket.API;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace HeadLamp
+{
+    public class LampSettings
+    {
+        [XmlAttribute] public ushort ItemID;
+        [XmlAttribute] public float DrainPerSecond; // Сколько % прочности тратится в сек.
+
+        public LampSettings() { }
+        public LampSettings(ushort id, float drain)
+        {
+            ItemID = id;
+            DrainPerSecond = drain;
+        }
+    }
+
+    public class HeadLampConfiguration : IRocketPluginConfiguration
+    {
+        public ushort BatteryItemID; // ID предмета-батарейки (по дефолту 337)
+        public List<LampSettings> Lamps;
+
+        public void Defaults()
+        {
+            BatteryItemID = 337;
+            Lamps = new List<LampSettings>
+            {
+                new LampSettings(1199, 0.5f), // Headlamp
+                new LampSettings(334, 0.2f)   // NVG
+            };
+        }
+    }
+}
