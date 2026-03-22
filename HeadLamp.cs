b8d0b8d8a5fb4729a775bcf96a327c2935a41a0f
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-22 20:57:11 +0300

    Update HeadLamp.cs

--- HeadLamp.cs
+++ HeadLamp.cs
@@ -2,7 +2,7 @@ using Rocket.Core.Plugins;
 using Rocket.Unturned;
 using HarmonyLib;
 using Rocket.Unturned.Player;
-using System;
+using SDG.Unturned;
 
 namespace HeadLamp
 {
@@ -18,19 +18,39 @@ namespace HeadLamp
             harmony.PatchAll();
 
             U.Events.OnPlayerConnected += OnPlayerConnected;
+
+            // Подхватываем игроков, которые УЖЕ на сервере (при /rocket reload)
+            foreach (var steamPlayer in Provider.clients)
+            {
+                UnturnedPlayer uPlayer = UnturnedPlayer.FromSteamPlayer(steamPlayer);
+                if (uPlayer.GameObject.GetComponent<PlayerLampComponent>() == null)
+                {
+                    uPlayer.GameObject.AddComponent<PlayerLampComponent>();
+                }
+            }
             
-            Rocket.Core.Logging.Logger.Log("HeadLamp Plugin Loaded!");
+            Rocket.Core.Logging.Logger.Log("HeadLamp Plugin Loaded successfully!");
         }
 
         protected override void Unload()
         {
             U.Events.OnPlayerConnected -= OnPlayerConnected;
             harmony.UnpatchAll("com.headlamp.patch");
+
+            // Зачищаем компоненты при выгрузке плагина
+            foreach (var steamPlayer in Provider.clients)
+            {
+                UnturnedPlayer uPlayer = UnturnedPlayer.FromSteamPlayer(steamPlayer);
+                var comp = uPlayer.GameObject.GetComponent<PlayerLampComponent>();
+                if (comp != null)
+                {
+                    UnityEngine.Object.Destroy(comp);
+                }
+            }
         }
 
         private void OnPlayerConnected(UnturnedPlayer player)
         {
-            // Добавляем компонент для отслеживания
             player.GameObject.AddComponent<PlayerLampComponent>();
         }
     }
