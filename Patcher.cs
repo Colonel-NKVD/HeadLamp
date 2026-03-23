e55a2a370ec869fb2fb0992be717cd566aad4f1a
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-23 16:08:39 +0300

    Update Patcher.cs

--- Patcher.cs
+++ Patcher.cs
@@ -4,20 +4,21 @@ using System.Linq;
 
 namespace HeadLamp
 {
-    // В современных версиях Unturned функция переключения называется ReceiveToggleVisual
     [HarmonyPatch(typeof(PlayerClothing), "ReceiveToggleVisual")]
     public class Patch_ToggleVisual
     {
         [HarmonyPrefix]
         public static bool Prefix(PlayerClothing __instance)
         {
-            if (!__instance.isVisualToggleActive) // Пытаются включить
+            // Проверяем, включен ли сейчас режим фонаря/ПНВ
+            // Если он ВЫКЛЮЧЕН (false), значит игрок пытается его ВКЛЮЧИТЬ
+            if (!__instance.isVisualToggleActive(EVisualToggleType.Headlamp)) 
             {
                 bool isHat = __instance.hatAsset != null && HeadLamp.Instance.Configuration.Instance.Lamps.Any(x => x.ItemID == __instance.hatAsset.id);
                 bool isGlasses = __instance.glassesAsset != null && HeadLamp.Instance.Configuration.Instance.Lamps.Any(x => x.ItemID == __instance.glassesAsset.id);
 
-                if (isHat && __instance.hatQuality <= 0) return false; // Запрещаем, если шапка сломана
-                if (isGlasses && __instance.glassesQuality <= 0) return false; // Запрещаем, если ПНВ сломано
+                if (isHat && __instance.hatQuality <= 0) return false; 
+                if (isGlasses && __instance.glassesQuality <= 0) return false;
             }
             return true;
         }
