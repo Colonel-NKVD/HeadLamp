00d0367f123870de78ac675fac06c61e3679afe7
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-23 16:38:03 +0300

    Update Patcher.cs

--- Patcher.cs
+++ Patcher.cs
@@ -4,30 +4,25 @@ using System.Linq;
 
 namespace HeadLamp
 {
-    // Патчим метод, который ты нашел. 
-    // Внимание: проверь в dnSpy, находится ли он в классе PlayerClothing или PlayerAnimate
-    // Скорее всего, это PlayerClothing.
-    [HarmonyPatch(typeof(PlayerClothing), "onGlassesUpdated")]
     public class Patch_onGlassesUpdated
     {
-        [HarmonyPrefix]
-        public static void Prefix(PlayerClothing __instance, ushort id, byte quality, ref byte[] state)
+        // Убираем [HarmonyPatch] - мы его вызвали вручную в HeadLamp.cs
+        public static bool Prefix(PlayerClothing __instance, ushort id, byte quality, byte[] state)
         {
-            // Если id не 0 (очки надеты) и прочность 0
+            // Если прочность 0 и это предмет из конфига
             if (id != 0 && quality <= 0)
             {
-                // Проверяем, есть ли предмет в нашем конфиге
                 var config = HeadLamp.Instance.Configuration.Instance.Lamps.FirstOrDefault(x => x.ItemID == id);
                 if (config != null)
                 {
-                    // state[0] отвечает за включение (1 - вкл, 0 - выкл)
-                    // Если кто-то пытается передать включенное состояние (1), принудительно ставим 0
+                    // Если пытаются включить (state[0] != 0), принудительно гасим в 0
                     if (state != null && state.Length > 0 && state[0] != 0)
                     {
-                        state[0] = 0; 
+                        state[0] = 0;
                     }
                 }
             }
+            return true; // Разрешаем выполнение оригинального метода с нашими правками в state
         }
     }
 }
